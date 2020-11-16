c6e159b2d6d0b597173c30eb563b1754443b0e05
auddnjs2008 <46766443+auddnjs2008@users.noreply.github.com>
2020-11-16 09:18:56 +0900

    setting

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import pocketmon from "pokemon-go-pokedex";
+
+
+function App() {
+  
+  console.log(pocketmon);
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Router.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {HashRouter as Router,
+        Route,
+        Switch,
+        Redirect,
+    } from "react-router-dom";
+
+
+export default ()=>{
+
+    return (
+        <Router>
+
+
+
+        </Router>
+    )
+
+
+}    
