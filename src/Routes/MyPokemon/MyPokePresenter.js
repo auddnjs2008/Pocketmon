2fef7b471dcfa5d24bc086260de0a50a8584ed16
auddnjs2008 <46766443+auddnjs2008@users.noreply.github.com>
2020-11-21 18:10:00 +0900

    my pokemon 작업중

--- src/Routes/MyPokemon/MyPokePresenter.js
+++ src/Routes/MyPokemon/MyPokePresenter.js
@@ -7,10 +7,13 @@ import ColorThief from "colorthief";
 
 const Container = styled.div``;
 const PokemonWrapper = styled.section`
+ background-color:rgba(0,0,0,0.8);
+padding:20px;
 border: 20px solid ${props => `rgba(${props.colors[0]},${props.colors[1]},${props.colors[2]})`};  
 display:grid;
-grid-template-columns:1fr 3fr;
-
+grid-template-columns:1fr 0.5fr 3fr;
+justify-items:center;
+align-items:center;
 `;
 const PokeImg = styled.img`
    width:auto;
@@ -44,11 +47,21 @@ align-items:center;
 const PokeInfo = styled.ul`
    display:grid;
    grid-auto-rows:1fr;
-   
-   li{
+   gap:5px;
+   color:white;
+   li.outLi{
       display:grid;
       grid-template-columns:2fr 3fr;
       text-align:center;
+      span:first-child{
+        
+         padding:5px;
+      }
+      ul{
+            display:grid;
+            grid-auto-flow:column;
+            grid-auto-columns:1fr;
+      }
      
    }
 
@@ -67,18 +80,87 @@ const Circle = styled.div`
 
 const CpBox =styled.div`
    width:50px;
-   height:100px;
+   height:120px;
+   background-color:white;
    position:relative;
-   border:1px solid black;
+    border:1px solid black;
+    border-top-left-radius:10px;
+    border-top-right-radius:10px;
+
+     overflow:hidden;
+
+     .cpTitle{
+        position:absolute;
+        bottom:0;
+        font-size:15px;
+        margin-bottom:5px;
+        span{
+           margin-right:5px;
+        }
+     }
  
 `
 const InnerBox = styled.div`
-     height:${props=>props.cp ? `${props.cp}%` : ""};
+    // height:${props=>props.cp ? `${props.cp}%` : ""};
      width:100%;
-     
      background-color:black;
      position:absolute;
-     bottom:0;
+     bottom:0; 
+     //background-color: ${props => `rgba(${props.colors[0]},${props.colors[1]},${props.colors[2]})`};  
+     background:${props => `linear-gradient(to bottom,rgba(${props.colors[0]},${props.colors[1]},${props.colors[2]}),white)`};
+
+   @keyframes fill{
+      0%{
+         height:0;
+      }
+      100%{
+     height:${props=>props.cp ? `${props.cp}%` : ""};
+      }
+   }
+   animation: fill 1s linear forwards;
+
+`;
+
+const InnerCircle = styled.div`
+   height:${props=>props.cp ? `${105-props.cp}%` : ""};
+   width:100%;
+   /* width:100px;
+   height:100px; */
+   //border: 1px solid black;
+   border-radius:50%;
+   position:absolute;
+   background-color:white;
+   top:0;
+   z-index:1;
+   @keyframes spin {
+      from{
+         transform:rotate(360deg);
+     
+      }
+   }
+
+   @keyframes fillWater{
+      0%{
+         height:100%;
+      }
+      100%{
+         height:${props=>props.cp ? `${105-props.cp}%` : ""};
+      }
+   }
+
+   animation: fillWater 3s , spin 4s linear infinite;
+   &:nth-child(1){
+      
+      left:-20px; 
+      animation: fillWater 3s,spin 3s linear infinite;
+
+   }
+   &:nth-child(2){
+      right:-20px;
+   animation:fillWater 3s, spin 5s linear infinite;
+
+   }
+
 `;
 
 
@@ -107,21 +189,24 @@ const MyPokePresenter = ({windowSize,pokemons,colorArray,setColor,handlePokemonC
          <Circle colors={colorArray}></Circle>   
          <h1>{item.name}</h1>
        </PokeProfile>
+       <CpBox>
+         <InnerCircle cp={parseInt(String(item.cp)[0] + String(item.cp)[1])}></InnerCircle>      
+
+         <InnerCircle cp={parseInt(String(item.cp)[0] + String(item.cp)[1])}></InnerCircle>      
+         <InnerCircle cp={parseInt(String(item.cp)[0] + String(item.cp)[1])}></InnerCircle>      
+         <InnerBox colors={colorArray}cp={parseInt(String(item.cp)[0] + String(item.cp)[1])}></InnerBox>   
+         <div className="cpTitle"><span>CP</span>{item.cp}</div>   
+      </CpBox>
       <PokeInfo>
-         <li><span>CP</span>{item.cp}</li>
-         <li><span>Type</span><span>{item.type.map(item=>item ? <span>{item}</span>:"")}</span></li>
-         <li><span>weight</span>{item.weight}</li>
-         <li><span>height</span>{item.height}</li>
-         <li><span>multipliers</span>{item.multipliers}</li>
-         <li><span>weaknesses</span><span>{item.weaknesses.map(item=>item ?<span>{item}</span> :"")}</span></li>   
-         {item.candy_count !== null ? <li><span>Candy-Number to evolve</span>{item.candy_count}</li> : <li>Can't Evolve</li>}      
+         <li className="outLi"><span>Type</span><ul>{item.type.map(item=>item ?<li>{item}</li>:"")}</ul></li>
+         <li className="outLi"><span>weight</span>{item.weight}</li>
+         <li className="outLi"><span>height</span>{item.height}</li>
+         <li className="outLi"><span>multipliers</span>{item.multipliers}</li>
+         <li className="outLi"><span>weaknesses</span><ul>{item.weaknesses.map(item=>item ?<li>{item}</li> :"")}</ul></li>   
+         {item.candy_count !== null ? <li className="outLi"><span>Candy-Number to evolve</span>{item.candy_count}</li> : <li>Can't Evolve</li>}      
 
       </PokeInfo>
-      <CpBox>
-    
-         <InnerBox ref={Wave}cp={parseInt(String(item.cp)[0] + String(item.cp)[1])}></InnerBox>   
-         <InnerBox ref={Wave}cp={parseInt(String(item.cp)[0] + String(item.cp)[1])}></InnerBox>      
-      </CpBox>  
+       
       </PokemonWrapper> 
       : "")}
     </Container>
